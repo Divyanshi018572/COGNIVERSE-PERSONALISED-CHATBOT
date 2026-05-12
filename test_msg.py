fcd4c7594be4f79802b9ad6b06839ea55eec2701
DIVYANSHI SINGH <2022011123@mmmut.ac.in>
2026-05-12 21:03:02 +0530

    chore: wipe root test scripts from github tracking

--- test_msg.py
+++
@@ -1,4 +0,0 @@
-from langchain_core.messages import AIMessage
-m = AIMessage(content="hello")
-print(getattr(m, "type", ""))
-print(type(m))
--- test_nvidia.py
+++
@@ -1,6 +0,0 @@
-from models.nvidia import get_llm
-from langchain_core.messages import HumanMessage
-
-llm = get_llm("nvidia/llama-3.3-nemotron-super-49b-v1")
-res = llm.invoke([HumanMessage(content="Hello")])
-print(repr(res.content))
